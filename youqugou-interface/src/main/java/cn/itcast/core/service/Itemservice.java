8f7c9094ab1ea492d4c267df4bddc8d090b3084e
Eleanore <Eleanore@163.com>
2019-01-18 00:10:35 +0800

    -- 马超

--- youqugou-interface/src/main/java/cn/itcast/core/service/Itemservice.java
+++ youqugou-interface/src/main/java/cn/itcast/core/service/Itemservice.java
@@ -3,7 +3,6 @@ package cn.itcast.core.service;
 import cn.itcast.core.pojo.item.Item;
 
 import java.util.List;
-import java.util.Map;
 
 public interface Itemservice {
     List<Item> findItemMapByGoodsId(Long id);
--- youqugou-service-sellergoods/src/main/java/cn/itcast/core/service/ItemserviceImpl.java
+++ youqugou-service-sellergoods/src/main/java/cn/itcast/core/service/ItemserviceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Transactional
